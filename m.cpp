4d17989023027aace36333160bae43f4351f0824
Akarsh-cy <cygnusd6@gmail.com>
2025-10-17 18:21:18 +0530

    logic correction

--- m.cpp
+++ m.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<cmath>
 #include<vector>
+#include<array>
 using std::array;
 
 
@@ -21,6 +22,12 @@ std::vector<std::array<float,2>> p;
 p.reserve(100);
 std::vector<std::array<float,2>> v;
 v.reserve(100);
+std::vector<std::array<float,2>> po2;
+p.reserve(100);
+std::vector<std::array<float,2>> vo2;
+v.reserve(100);
+
+
 
 
 for(int i=0;i<99;++i){
@@ -31,11 +38,26 @@ for(int i=0;i<99;++i){
 np1[0]=p1[0]+v1[0]*t+0.5*t*t*(p2[0]-p1[0])/k;
 np1[1]=p1[1]+v1[1]*t+0.5*t*t*(p2[1]-p1[1])/k;
 
+
+np2[0]=p2[0]+v2[0]*t+0.5*t*t*(p1[0]-p2[0])/k;
+np2[1]=p2[1]+v2[1]*t+0.5*t*t*(p1[1]-p2[1])/k;
+
+
+
 nv1[0]=v1[0]+((p2[0]-p1[0])/k)*t;
 nv1[1]=v1[1]+((p2[1]-p1[1])/k)*t;
 
+nv2[0]=v2[0]+((p1[0]-p2[0])/k)*t;
+nv2[1]=v2[1]+((p1[1]-p2[1])/k)*t;
+
+
+
 p.push_back(np1);
 v.push_back(nv1);
+po2.push_back(np2);
+vo2.push_back(nv2);
+
+
 
 p1=np1;
 v1=nv1;
@@ -43,15 +65,27 @@ p2=np2;
 v2=nv2;
   }//for loop ends
 
-std::cout<<"positions:\n";
+std::cout<<"positions of 1:\n";
 for(auto item:p){
     std::cout<<item[0]<<" , "
              <<item[1]<<"\n";}
 
-std::cout<<"velocity:\n";
+std::cout<<"velocity of 1:\n";
 for(auto item:v){
     std::cout<<item[0]<<" , "
              <<item[1]<<"\n";}
+
+std::cout<<"positions of 2:\n";
+for(auto item:po2){
+    std::cout<<item[0]<<" , "
+             <<item[1]<<"\n";}
+
+std::cout<<"velocity of 2:\n";
+for(auto item:vo2){
+    std::cout<<item[0]<<" , "
+             <<item[1]<<"\n";}
+
+
 
 
 
